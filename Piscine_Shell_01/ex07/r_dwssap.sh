3c8b419e7b9caae8490011b878ff2c076d9c980a
MarcoApunto <91089291+MarcoApunto@users.noreply.github.com>
2021-09-27 21:41:42 +0200

    Update r_dwssap.sh
    Don't work.

--- Piscine_Shell_01/ex07/r_dwssap.sh
+++ Piscine_Shell_01/ex07/r_dwssap.sh
@@ -1,3 +1,1 @@
-Don,t work well.
-
 cat /etc/passwd | grep -v "#" | awk 'NR%2==0' | rev | cut -d ':' -f 7 | sort -r | awk -v l1=$FT_LINE1 -v l2=$FT_LINE2 'NR==l1,NR==l2' | tr "\n" "," | sed 's/,$/./' | sed 's/, */, /g'
